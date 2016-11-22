0e0cbb51a798925750f745ddec02eeed302905eb
Nicolas Lecourtois <lecourtoisn@gmail.com>
2016-11-22 10:33:34 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+cookies = 0
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/cookies")
+def getCookies():
+	global cookies
+	cookies += 1
+	return "I have {} cookies !".format(cookies)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=4242)
