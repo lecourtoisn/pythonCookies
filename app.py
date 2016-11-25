50a0368e97a8248f956049eaa872b0a8a43506f5
Nicolas Lecourtois <lecourtoisn@gmail.com>
2016-11-25 16:28:10 +0100

    Python standard indentation

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 cookies = 0
 
+
 @app.route("/")
 def hello():
-    return "Hello cookie eater! Hope you're doing fine!"
+    return "Hello cookie eater friend! Hope you're doing fine!"
+
+
 @app.route("/cookies")
 def getCookies():
-	global cookies
-	cookies += 1
-	return "I have {} cookies !".format(cookies)
+    global cookies
+    cookies += 1
+    return "I have {} cookies !".format(cookies)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
